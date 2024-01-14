3a9a37ae62c6e365a7828829543889cb27b071c9
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-01-14 12:04:38 +0700

    Hoàn thành xong footer Desktop

--- src/Redux/footerSlice.js
+++ src/Redux/footerSlice.js
@@ -1,19 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+// import { https } from "../Service/api";
+
+// export const footerSliceAction = createAsyncThunk(
+//   "footerSliceAction",
+//   async () => {
+//     let res = await https.get("/api/QuanLyRap/LayThongTinHeThongRap");
+//     console.log("🚀 ~ footerSliceAction ~ res:", res);
+//     return res.data.content;
+//   }
+// );
 
 const initialState = {
-    infoRap: [],
-}
+  infoRap: [],
+};
 
 const footerSlice = createSlice({
   name: "footerSlice",
   initialState,
   reducers: {
-    setInfoRap: (state, action)=>{
+    setInfoRap: (state, action) => {
       state.infoRap = action.payload;
     },
-  }
+  },
+  // extraReducers: {
+  //   [footerSliceAction.fulfilled]: (state, action) => {
+  //     state.infoRap = action.payload;
+  //   },
+  //   [footerSliceAction.pending]: (state, action) => {
+  //     state.infoRap = action.payload;
+  //   },
+  // },
 });
 
-export const { setInfoRap } = footerSlice.actions
+export const { setInfoRap } = footerSlice.actions;
 
-export default footerSlice.reducer
+export default footerSlice.reducer;
